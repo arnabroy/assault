54cd1af7543f8512b1171a6e7578c1ebdfd0ffb8
Arnab Roy <arnabroy.007@gmail.com>
2019-11-02 01:41:21 -0700

    updated http.py

--- assault/http.py
+++ assault/http.py
@@ -1,15 +1,15 @@
 import asyncio
 
-# Make the request and return the results
 def fetch(url):
+    """ Make request and return result """
     pass
 
-# A function to take unmade requests from a queue, perform the work, and add result to the queue
-def worker(name, queue, results)
+def worker(name, queue, results):
+    """ A function to take unmade requests from a queue, perform the work, and add result to the queue """
     pass
 
-# Divide up work into batches and collect final results
-def distribute_work(url, requests, concurrency, results):
+async def distribute_work(url, requests, concurrency, results):
+    """ Divide up work into batches and collect final results """
     queue = asyncio.Queue()
 
     # Add an item to the queue for each request we want to make
@@ -34,8 +34,8 @@ def distribute_work(url, requests, concurrency, results):
         f"{concurrency} workers took {total_time:.2f} seconds to complete {len(results)} requests"
     )
 
-# Entrypoint to making requests
 def assault(url, requests, concurrency):
+    """ Entrypoint to making requests """
     results = []
     asyncio.run(distribute_work(url, requests, concurrency, results))
     print(results)
