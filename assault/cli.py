289647d189a9e8763e5f4b97a0ed7bed900265a1
Arnab Roy <arnabroy.007@gmail.com>
2019-11-05 00:19:50 -0800

    added config.py

--- assault/cli.py
+++ assault/cli.py
@@ -1,6 +1,8 @@
 import click
 
 from .http import assault
+from .stats import Results
+
 
 @click.command()
 @click.option("--requests", "-r", default=500, help="Number of requests")
@@ -8,11 +10,22 @@ from .http import assault
 @click.option("--json-file", "-j", default=None, help="Path to output JSON file")
 @click.argument("url")
 def cli(requests, concurrency, json_file, url):
-    print(f"Requests: {requests}")
-    print(f"Concurrency: {concurrency}")
-    print(f"JSON File: {json_file}")
-    print(f"URL: {url}")
-    assault(url, requests, concurrency)
+    total_time, request_dicts = assault(url, requests, concurrency)
+    results = Results(total_time, request_dicts)
+    display(results, json_file)
 
-if __name__ == "__main__":
-    cli()
+
+def display(results, json_file):
+    if json_file:
+        # Write to a file
+        print("We're writing to a JSON file")
+    else:
+        # Print to screen
+        print(".... Done!")
+        print("--- Results ---")
+        print(f"Successful Requests\t{results.successful_requests()}")
+        print(f"Slowest            \t{results.slowest()}s")
+        print(f"Fastest            \t{results.fastest()}s")
+        print(f"Total time         \t{results.total_time}s")
+        print(f"Requests Per Minute\t{results.requests_per_minute()}")
+        print(f"Requests Per Second\t{results.requests_per_second()}")
